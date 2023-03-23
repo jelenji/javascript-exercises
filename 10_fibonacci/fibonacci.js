9da7cf14970b611dafd92ddf7b593c8cf012844b
Jalal <jalaljcharles@gmail.com>
2023-03-23 14:05:38 -0400

    finished up to exercise 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    var num1=0;
+    var num2=1;
+    var sum;
+    for (let i = 1; i < num; i++) 
+    {
+        sum=num1+num2;
+        num1=num2;
+        num2=sum;
+    }
+    return num2;
 };
 
 // Do not edit below this line
