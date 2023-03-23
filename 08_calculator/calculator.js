17c87101521a907a1f28ad9dfd75ed8923877fee
Jalal <jalaljcharles@gmail.com>
2023-03-23 04:36:24 -0400

    completed up to exercise 10

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const sum = arr.reduce((total, item) =>{
+    return total + item;
+  }, 0)
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  const product = arr.reduce((total, item) =>{
+    return total * item;
+  }, 1)
 
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return Math.pow(num, pow);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  return (num > 1) ? num * factorial(num - 1) : 1;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let noPunc = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,""); //remove punctiation for string
+    noPunc = noPunc.replaceAll(' ', '').toLowerCase();
+
+    console.log(noPunc);
+
+    return [...noPunc].reverse().join("") === noPunc ? true : false; // spread string reverse and compare 
 };
 
 // Do not edit below this line
